f7afdafceb14d48bda2e82b0243d3088d8bcbf2b
Ivanildo <Ivanildo Borges>
2021-06-30 13:19:10 -0300

    Alteração da rota serviços

--- routes/funcionarios.js
+++ routes/funcionarios.js
@@ -1,62 +1,44 @@
 const express = require("express");
+const nodemon = require('nodemon');
 const router = express.Router();
+const mysql = require('../database/mysql').pool;
 
 //RETORNA TODOS OS FUNCIONARIOS
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Lista de todos os funcionarios"
+    mysql.getConnection((error, conn) => {
+        if (error) { 
+            return res.status(500).send({ error: error });
+        }
+        conn.query(
+            'SELECT * FROM funcionarios;',
+            (error, resultado, fields) => {
+                if (error) { 
+                    return res.status(500).send({ error: error });
+                }
+                return res.status(200).send({ response: resultado });
+            }
+        );
     });
 });
 
 //INSERE UM FUNCIONARIO
 router.post('/', (req, res, next) => {
-    const funcionario = {
-        nome: req.body.nome,
-        empresa: req.body.empresa,
-        cargo: req.body.cargo,
-        especializacao: req.body.especializacao
-    }
-
-    res.status(201).send({
-        mensagem: "Funcionario inserido com sucesso!",
-        funcionarioCriado: funcionario
-    }); 
+     
 });
 
 //RETORNA UM FUNCIONARIO ESPECIFICO
 router.get('/:id', (req, res, next) => {
-    const id = req.params.id;
-
-    if (id == "1") {
-        res.status(200).send({
-            mensagem: "Funcionario 1"
-        });
-    } else {
-        res.status(200).send({
-            mensagem: "Funcionario não encontrado"
-        });
-    }
+    
 });
 
 //ATUALIZA UM FUNCIONARIO
-router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Funcionario alterado com sucesso!"
-    });
+router.put('/', (req, res, next) => {
+    
 });
 
 //DELETA CONTA FUNCIONARIO
 router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    if (id == "1") {
-        res.status(201).send({
-            mensagem: "Funcionario 1 apagado!"
-        });
-    } else {
-        res.status(200).send({
-            mensagem: "Funcionario não encontrado"
-        });
-    }
+    
 });
 
 //Exportando a rota
--- routes/servicos.js
+++ routes/servicos.js
@@ -3,18 +3,21 @@ const nodemon = require('nodemon');
 const router = express.Router();
 const mysql = require('../database/mysql').pool;
 
-
 // LISTAR TODOS SERVIÇOS:
 router.get('/', (req, res, next) => {
-    mysql.getConnection((error, conn) =>{
-        if(error) { return res.status(500).send({ error: error}) }
+    mysql.getConnection((error, conn) => {
+        if (error) { 
+            return res.status(500).send({ error: error});
+        }
         conn.query(
             'SELECT * FROM servicos;',
-            (error, resultado, fields) =>{
-                if(error) { return res.status(500).send({ error: error}) }
+            (error, resultado, fields) => {
+                if (error) { 
+                    return res.status(500).send({ error: error});
+                }
                 return res.status(200).send({response: resultado})
             }
-        )
+        );
     });
 });
 
@@ -22,7 +25,7 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
     mysql.getConnection((error, conn) => { 
         if (error) { 
-            return res.status(500).send({ error: error })
+            return res.status(500).send({ error: error });
         }
 
         conn.query(
@@ -32,7 +35,7 @@ router.get('/:id', (req, res, next) => {
                 if (error) {
                     return res.status(500).send({ error: error })
                 }
-                return res.status(500).send({ response: resultado });
+                return res.status(200).send({ response: resultado });
             }
         );
     });
@@ -48,7 +51,7 @@ router.post('/', (req, res, next) => {
         }
         conn.query(
             'INSERT INTO servicos (nome, categoria, descricao) VALUES (?,?,?)',
-            [req.params.nome, req.params.categoria,req.params.descricao],
+            [req.body.nome, req.body.categoria, req.body.descricao],
             (error, resultado, field) => {
                 conn.release();
 
@@ -69,7 +72,7 @@ router.post('/', (req, res, next) => {
 
 
 // ALTERAR UM PRODUTO:
-router.patch('/', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if(error) {
             return res.status(500).send({ error: error})
@@ -79,9 +82,9 @@ router.patch('/', (req, res, next) => {
             SET nome = ?, categoria = ?, descricao = ?
             WHERE id = ?`,
             [
-                req.params.nome,
-                req.params.categoria, 
-                req.params.descricao,
+                req.body.nome,
+                req.body.categoria, 
+                req.body.descricao,
                 req.params.id
             ],
             (error, resultado, field) => {
@@ -97,7 +100,7 @@ router.patch('/', (req, res, next) => {
 });
 
 //DELETAR UM SERVIÇO
-router.delete('/', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) {
             return res.status(500).send({ error: error })
